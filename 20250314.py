a8349e8f9595dbc98bd2ec26a4a45279b65637b0
Seojoon-S <Leon031212@gmail.com>
2025-03-14 20:54:45 +0900

    Add files via upload

---
+++ 20250314.py
@@ -0,0 +1,43 @@
+a=1
+print(a)
+a=123
+b="Hello World"
+c=True
+d=123.456
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+print(d, type(d))
+print("%d는 정수이다." %7)
+number = 3
+print("%d은 정수이다." %number)
+m=3
+d=14
+print("오늘은 %d월 %d일이다." % (m,d))
+x=1
+y=2
+z=21
+print("나는 %d학년 %d반 %d번입니다. " % (x,y,z))
+pi=3.141592
+print("%lf" % pi)
+print("%.2lf" % pi)
+a=1
+b=10
+c=100
+d=2
+e=20
+f=200
+print("%-3d" "%4d" %(a,d))
+print("%-3d" "%4d" %(b,e))
+print("%-3d" "%4d" %(c,f))
+a=1
+b=10
+print("%s %s" %(a,b))
+name="이서준"
+print("내 이름은 {0}이고, {1}월에 태어났다.".format(name,3))
+name=input("이름 : ")
+print("==자기 소개하기==")
+print("이름 : %s" % name)
+a=int(input())
+b=int(input())
+print("%d+%d=%d"%(a,b, a+b))
